2ffb5ebd8c8af8de9bab52e47e6605c95afa977a
siddhant-deshmukh <siddhant946044@gmail.com>
2024-04-13 13:24:56 +0530

    added /about and /contact

--- src/data/seed.mjs
+++ src/data/seed.mjs
@@ -46,3 +46,29 @@ const jsonData = JSON.stringify(blogs, null, 2);
 // Write the JSON data to a file
 fs.writeFileSync('src/data/blogs.json', jsonData);
 console.log('JSON data written to blogs.json');
+
+
+
+const numMembers = 5;
+
+// Create an array to store the blog data
+const members = [];
+
+// Generate fake data for each blog
+for (let i = 0; i < numMembers; i++) {
+  const member = {
+    avatar: faker.image.avatar(),
+    name: faker.person.fullName(),
+    jobTitle: faker.person.jobTitle(),
+    jobDescription: faker.lorem.sentence(),
+  };
+
+  members.push(member);
+}
+
+// Convert the array of members to JSON
+const membersJsonData = JSON.stringify(members, null, 2);
+
+// Write the JSON data to a file
+fs.writeFileSync('src/data/members.json', membersJsonData);
+console.log('JSON data written to members.json');
